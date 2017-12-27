10688611d7bbd69b86fc63b5a145049e541b0ecf
Max Strobel <max.strobel28@gmail.com>
2017-12-27 21:07:33 +0100

    added a DQN prototype added preprocessing tools + demo

---
+++ dqn.py
@@ -0,0 +1,91 @@
+"""DQN"""
+"""
+ Sources
+
+"""
+import torch
+import torch.nn as nn
+
+
+class DQN(nn.Module):
+
+    def __init__(self, num_actions=4):
+        super(DQN, self).__init__()
+
+        # Input 84x84
+        self.conv_block1 = nn.Sequential(
+                nn.Conv2d(in_channels=1,
+                          out_channels=32,
+                          kernel_size=8,
+                          stride=4),
+                nn.ReLU(inplace=True)
+                )
+        self.conv_block2 = nn.Sequential(
+                nn.Conv2d(in_channels=32,
+                          out_channels=64,
+                          kernel_size=4,
+                          stride=2),
+                nn.ReLU(inplace=True)
+                )
+        self.conv_block3 = nn.Sequential(
+                nn.Conv2d(in_channels=64,
+                          out_channels=64,
+                          kernel_size=3,
+                          stride=1),
+                nn.ReLU(inplace=True)
+                )
+        self.fc1 = nn.Sequential(
+                nn.Linear(in_features=64*7*7,
+                          out_features=512),
+                nn.ReLU(inplace=True)
+                )
+        self.fc2 = nn.Linear(
+                in_features=512,
+                out_features=num_actions
+                )
+
+
+    def forward(self, x):
+        """
+        Forward pass of the dqn. Should not be called
+        manually but by calling a model instance directly.
+
+        Inputs:
+        - x: PyTorch input Variable
+        """
+        (N, C, H, W) = x.size()
+        
+        x = self.conv_block1(x)
+        #print('conv1',x.size())
+        x = self.conv_block2(x)
+        #print('conv2',x.size())
+        x = self.conv_block3(x)
+        #print('conv3',x.size())
+        x = x.view(N,-1) # change the view from 2d to 1d
+        #print('conv3_flat', x.size())
+        x = self.fc1(x)
+        #print('fc1',x.size())
+        x = self.fc2(x)
+        #print('score',x.size())
+
+        
+        return x
+    
+    
+    @property
+    def is_cuda(self):
+        """
+        Check if model parameters are allocated on the GPU.
+        """
+        return next(self.parameters()).is_cuda
+
+    def save(self, path):
+        """
+        Save model with its parameters to the given path. Conventionally the
+        path should end with "*.model".
+
+        Inputs:
+        - path: path string
+        """
+        print('Saving model... %s' % path)
+        torch.save(self, path)
---
+++ utils.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 27 13:55:50 2017
+
+@author: max
+"""
+
+import numpy as np
+from PIL import Image
+import torch
+import torchvision.transforms as T
+
+
+
+# if gpu is to be used
+use_cuda = torch.cuda.is_available()
+FloatTensor = torch.cuda.FloatTensor if use_cuda else torch.FloatTensor
+LongTensor = torch.cuda.LongTensor if use_cuda else torch.LongTensor
+ByteTensor = torch.cuda.ByteTensor if use_cuda else torch.ByteTensor
+Tensor = FloatTensor
+
+
+
+
+
+# Resize an image to output_size
+output_size = (84,84)
+resize = T.Compose([T.ToPILImage(),
+                    T.Resize(output_size, interpolation=Image.CUBIC),
+                    T.ToTensor()])
+
+def get_screen(env):
+    """
+    get current screen of breakout and do simple preprocessing
+    """
+    # TODO: Parametrize strip coordinates
+    
+    # transpose into torch order (CHW)
+    screen = env.render(mode='rgb_array').transpose((2, 0, 1))
+    
+    # Strip screen at the edges => checked via RGB values of the wall
+    screen = screen[:, 32:195, 8:152]
+    
+    # Convert to float, rescare, convert to torch tensor
+    # (this doesn't require a copy)
+    screen = np.ascontiguousarray(screen, dtype=np.float32) / 255
+    screen = torch.from_numpy(screen)
+
+    return screen.unsqueeze(0).type(Tensor)
+
+def get_screen_resize(env):
+    """
+    get current screen of breakout and do simple preprocessing
+    """
+    # TODO: Parametrize strip coordinates
+    
+    # transpose into torch order (CHW)
+    screen = env.render(mode='rgb_array').transpose((2, 0, 1))
+    
+    # Strip screen at the edges => checked via RGB values of the wall
+    screen = screen[:, 32:195, 8:152]
+    
+    # Convert to float, rescale, convert to torch tensor
+    # (this doesn't require a copy)
+    screen = np.ascontiguousarray(screen, dtype=np.float32) / 255
+    screen = torch.from_numpy(screen)
+    # Resize, and add a batch dimension (BCHW)
+    return resize(screen).unsqueeze(0).type(Tensor)
+
+def rgb2gray(img):
+    """
+    convert rgb to grayscale by averaging channel intensities
+    """
+
+    n, ch, w, h = img.size()
+    
+    r = img[:,0,:,:].unsqueeze(1)
+    g = img[:,1,:,:].unsqueeze(1)
+    b = img[:,2,:,:].unsqueeze(1)
+    
+    z = torch.Tensor(n,1,w,h).zero_()
+    
+    z = 0.21*r + 0.72*g + 0.07*b
+    return z
+
+def rgb2y(img):
+    """
+    convert rgb to luminance (wikipedia)
+    """
+
+    n, ch, w, h = img.size()
+    
+    r = img[:,0,:,:].unsqueeze(1)
+    g = img[:,1,:,:].unsqueeze(1)
+    b = img[:,2,:,:].unsqueeze(1)
+    
+    z = torch.Tensor(n,1,w,h).zero_()
+    
+    z = 0.299*r + 0.587*g + 0.114*b
+    return z
+
+def rgb2bw(img):
+    """
+    convert rgb to black white by checking zero values
+    """
+    
+    n, ch, w, h = img.size()
+        
+    r = img[:,0,:,:].unsqueeze(1)
+    g = img[:,1,:,:].unsqueeze(1)
+    b = img[:,2,:,:].unsqueeze(1)
+    
+    z = torch.Tensor(n,1,w,h).zero_()
+    
+    z = (r+g+b)!=0
+    return z
