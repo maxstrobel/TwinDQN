f8eb9293fbad2fa7eb81772f793e0037770fbb46
Max Strobel <max.strobel28@gmail.com>
2018-01-16 22:20:56 +0100

    move actions to readme.txt

--- environment.py
+++ environment.py
@@ -6,29 +6,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from cv2 import resize
 
-"""
-ACTION_MEANING = {
-    0 : "NOOP",
-    1 : "FIRE",
-    2 : "UP",
-    3 : "RIGHT",
-    4 : "LEFT",
-    5 : "DOWN",
-    6 : "UPRIGHT",
-    7 : "UPLEFT",
-    8 : "DOWNRIGHT",
-    9 : "DOWNLEFT",
-    10 : "UPFIRE",
-    11 : "RIGHTFIRE",
-    12 : "LEFTFIRE",
-    13 : "DOWNFIRE",
-    14 : "UPRIGHTFIRE",
-    15 : "UPLEFTFIRE",
-    16 : "DOWNRIGHTFIRE",
-    17 : "DOWNLEFTFIRE",
-}
-"""
-
 class Environment(object):
     def __init__(self, game, inner_dimensions, frameskip=4, width=84, height=84):
         """
