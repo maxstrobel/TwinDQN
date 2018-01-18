61a0c357c85c6d37c24ad21550b2c7bebd384813
Max Strobel <max.strobel28@gmail.com>
2018-01-18 16:15:04 +0100

    new folder for single game

---
+++ single_game/dqn.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class DQN(nn.Module):
+    def __init__(self, channels_in, num_actions):
+        super(DQN, self).__init__()
+
+        self.conv1 = nn.Conv2d(in_channels=channels_in,
+                          out_channels=32,
+                          kernel_size=8,
+                          stride=4)
+        self.conv2 = nn.Conv2d(in_channels=32,
+                          out_channels=64,
+                          kernel_size=4,
+                          stride=2)
+        self.conv3 = nn.Conv2d(in_channels=64,
+                          out_channels=64,
+                          kernel_size=3,
+                          stride=1)
+        self.fc4 = nn.Linear(in_features=64*7*7,
+                          out_features=512)
+        self.fc5 = nn.Linear(in_features=512,
+                          out_features=num_actions)
+
+
+    def forward(self, x):
+        """
+        Forward pass of the dqn. Should not be called
+        manually but by calling a model instance directly.
+
+        Inputs:
+        - x: PyTorch input Variable
+        """
+        N, C, H, W = x.size()
+        #print('forward',H,W)
+        x = F.relu(self.conv1(x))
+        #print('conv1',x.size())
+        x = F.relu(self.conv2(x))
+        #print('conv2',x.size())
+        x = F.relu(self.conv3(x))
+        #print('conv3',x.size())
+        x = x.view(N,-1) # change the view from 2d to 1d
+        #print('conv3_flat', x.size())
+        x = F.relu(self.fc4(x))
+        #print('fc1',x.size())
+        x = self.fc5(x)
+        #print('score',x.size())
+
+
+        return x
+
+
+    @property
+    def is_cuda(self):
+        """
+        Check if model parameters are allocated on the GPU.
+        """
+        return next(self.parameters()).is_cuda
+
+
+    def save(self, path):
+        """
+        Save model with its parameters to the given path. Conventionally the
+        path should end with "*.model".
+
+        Inputs:
+        - path: path string
+        """
+        print('Saving model... %s' % path)
+        torch.save(self.state_dict(), path)
+
+
+    def load(self, path):
+        """
+        Load model with its parameters from the given path. Conventionally the
+        path should end with "*.model".
+
+        Inputs:
+        - path: path string
+        """
+        print('Loading model... %s' % path)
+        self.load_state_dict(torch.load(path))
---
+++ single_game/replaymemory.py
@@ -0,0 +1,65 @@
+import torch
+from collections import namedtuple
+import random
+
+Transition = namedtuple('Transition', ('state','action','next_state','reward'))
+TransitionIdx = namedtuple('transitionIdx', ('idx', 'action', 'reward', 'done'))
+
+USE_CUDA = torch.cuda.is_available()
+tType = torch.cuda.FloatTensor if USE_CUDA else torch.FloatTensor
+
+class ReplayMemory(object):
+    def __init__(self, capacity, num_history_frames = 4):
+        self.capacity = capacity
+        self.memory = []
+        self.memoryTransitions = []
+        self.num_frames = 0
+        self.memory_full = False
+        self.num_transitions = 0
+        self.num_history = num_history_frames
+
+    def getCurrentIndex(self):
+        return (self.num_frames-1)%self.capacity
+
+    def pushTransition(self,*args):
+        if len(self.memoryTransitions) < self.capacity-1:
+            self.memoryTransitions.append(None)
+        self.memoryTransitions[self.num_transitions] = TransitionIdx(*args)
+        self.num_transitions = (self.num_transitions+1)% (self.capacity-1)
+
+    def pushFrame(self, frame):
+        if len(self.memory)< self.capacity:
+            self.memory.append(None)
+        else:
+            self.memory_full = True
+        self.memory[self.num_frames] = frame
+        self.num_frames = (self.num_frames +1)% self.capacity
+
+    def sampleTransition(self, batch_size):
+        rnd_transitions = random.sample(self.memoryTransitions, batch_size)
+        output = []
+        for i in range(len(rnd_transitions)):
+            state = self.memory[rnd_transitions[i][0]]
+            for j in range(self.num_history-1):
+                idx = rnd_transitions[i][0]-1-j
+                if not self.memory_full:
+                    idx = max(0, idx)
+                state = torch.cat((self.memory[(idx)%self.capacity], state),1)
+
+            action = rnd_transitions[i][1]
+            reward = rnd_transitions[i][2]
+            output.append(None)
+            if rnd_transitions[i][3]:
+                output[i] = Transition(state.type(tType)/255.0, action, None, reward)
+            else:
+                next_state = self.memory[(rnd_transitions[i][0]+1)%self.capacity]
+                for j in range(self.num_history-1):
+                    idx =  rnd_transitions[i][0]-j
+                    if not self.memory_full:
+                        idx = max(0, idx)
+                    next_state = torch.cat((self.memory[(idx)%self.capacity], next_state),1)
+                output[i] = Transition(state.type(tType)/255.0, action, next_state.type(tType)/255.0, reward)
+        return Transition(*zip(* output))
+
+    def __len__(self):
+        return len(self.memory)
